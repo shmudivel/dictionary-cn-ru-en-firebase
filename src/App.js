f9cfe5a4b744f61724252cff0869c0126d81f7f8
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-09-28 10:30:59 -0400

    form show/hide added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 //import { AiOutlinePlus } from 'react-icons/ai';
-import Word from './Word';
-import { db } from './firebase';
+import Word from "./Word";
+import { db } from "./firebase";
 import {
   query,
   collection,
   onSnapshot,
   updateDoc,
   doc,
-  addDoc,
   deleteDoc,
-} from 'firebase/firestore';
+} from "firebase/firestore";
+import NewWordForm from "./components/NewWordForm";
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
@@ -24,36 +24,39 @@ const style = {
 
 function App() {
   const [words, setWords] = useState([]);
-  const [english, setEnglish] = useState('');
-  const [russian, setRussian] = useState('');
-  const [chinese, setChinese] = useState('');
-  const [pinyin, setPinyin] = useState('');
-  // Search 
-  const [searchTerm, setSearchTerm] = useState('')
+  // const [english, setEnglish] = useState('');
+  // const [russian, setRussian] = useState('');
+  // const [chinese, setChinese] = useState('');
+  // const [pinyin, setPinyin] = useState('');
+  // Search
+  const [searchTerm, setSearchTerm] = useState("");
 
-  // Create word
-  const createWord = async (e) => {
-    e.preventDefault(e);
-    if (english === '' || russian === '' || chinese === '' || pinyin === '') {
-      alert('Please enter a word');
-      return;
-    }
-    await addDoc(collection(db, 'words'), {
-      english: english,
-      russian: russian,
-      chinese: chinese,
-      pinyin: pinyin,
-      completed: false,
-    });
-    setEnglish('');
-    setRussian('');
-    setChinese('');
-    setPinyin('');
-  };
+  // show form button
+  const [isForm, setIsForm] = useState(false);
+
+  // // Create word
+  // const createWord = async (e) => {
+  //   e.preventDefault(e);
+  //   if (english === '' || russian === '' || chinese === '' || pinyin === '') {
+  //     alert('Please enter a word');
+  //     return;
+  //   }
+  //   await addDoc(collection(db, 'words'), {
+  //     english: english,
+  //     russian: russian,
+  //     chinese: chinese,
+  //     pinyin: pinyin,
+  //     completed: false,
+  //   });
+  //   setEnglish('');
+  //   setRussian('');
+  //   setChinese('');
+  //   setPinyin('');
+  // };
 
   // Read 'word' from firebase
   useEffect(() => {
-    const q = query(collection(db, 'words'));
+    const q = query(collection(db, "words"));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       let wordsArr = [];
       querySnapshot.forEach((doc) => {
@@ -66,94 +69,68 @@ function App() {
 
   // Update word in firebase
   const toggleComplete = async (word) => {
-    await updateDoc(doc(db, 'words', word.id), {
+    await updateDoc(doc(db, "words", word.id), {
       completed: !word.completed,
     });
   };
 
   // Delete word
   const deleteWord = async (id) => {
-    await deleteDoc(doc(db, 'words', id));
+    await deleteDoc(doc(db, "words", id));
   };
-
-
 
   return (
     <div className={style.bg}>
       <div className={style.container}>
         <h3 className={style.heading}>Words App</h3>
-        <form onSubmit={createWord} className={style.form}>
+        <button
+          className={style.button}
+          onClick={() => {
+            setIsForm(!isForm);
+          }}
+        >
+          {isForm ? "Close form" : "Add a new word"}
+        </button>
 
-          <input
-            value={english}
-            onChange={(e) => setEnglish(e.target.value)}
-            className={style.input}
-            type='text'
-            placeholder='English word'
-          />
-                    
-          <input
-            value={russian}
-            onChange={(e) => setRussian(e.target.value)}
-            className={style.input}
-            type='text'
-            placeholder='Russian word'
-          />
+        {isForm ? <NewWordForm /> : " "}
 
-          <input
-            value={chinese}
-            onChange={(e) => setChinese(e.target.value)}
-            className={style.input}
-            type='text'
-            placeholder='Chinese word'
-          />
-
-          <input
-            value={pinyin}
-            onChange={(e) => setPinyin(e.target.value)}
-            className={style.input}
-            type='text'
-            placeholder='Pinyin'
-          />
-
-          <button className={style.button}>
-             Submit
-          </button>
-        </form>
         <ul>
-          <input 
-          onChange={event => {
-            setSearchTerm(event.target.value)
-            }} 
-            className={style.input} 
-            type="text" 
-            placeholder="Search..."  
+          <input
+            onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}
+            className={style.input}
+            type="text"
+            placeholder="Search..."
           />
 
-          {words.filter((word) => {
-            if (searchTerm === "") {
-              
-            } else if (
-              word.english.toLowerCase().includes(searchTerm.toLowerCase())
-              || word.russian.toLowerCase().includes(searchTerm.toLowerCase())
-              || word.chinese.toLowerCase().includes(searchTerm.toLowerCase())
+          {words
+            .filter((word) => {
+              if (searchTerm === "") {
+              } else if (
+                word.english.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                word.russian.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                word.chinese.toLowerCase().includes(searchTerm.toLowerCase())
               ) {
-              return word
-            }
-            return ''
-          }).map((word, index) => {
-          return (
-            <Word
-              key={index}
-              word={word}
-              toggleComplete={toggleComplete}
-              deleteWord={deleteWord}
-            />
-          );
-          })}
+                return word;
+              }
+              return "";
+            })
+            .map((word, index) => {
+              return (
+                <Word
+                  key={index}
+                  word={word}
+                  toggleComplete={toggleComplete}
+                  deleteWord={deleteWord}
+                />
+              );
+            })}
         </ul>
         {words.length < 1 ? null : (
-          <p className={style.count}>{`Dictionary have ${words.length} words`}</p>
+          <p
+            className={style.count}
+          >{`Dictionary have ${words.length} words`}</p>
         )}
       </div>
     </div>
---
+++ src/components/NewWordForm.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { db } from '../firebase';
+import { 
+    collection,
+    addDoc, 
+} from "firebase/firestore";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+  container: `bg-slate-100 max-w-[600px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `p-2 justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border text-2xl p-2 w-full bg-purple-500 hover:bg-purple-600 text-slate-100`,
+  count: `text-center p-2`,
+};
+
+const NewWordForm = () => {
+    // States
+    const [english, setEnglish] = useState('');
+    const [russian, setRussian] = useState('');
+    const [chinese, setChinese] = useState('');
+    const [pinyin, setPinyin] = useState('');
+
+
+
+      // Create word
+  const createWord = async (e) => {
+    e.preventDefault(e);
+    if (english === '' || russian === '' || chinese === '' || pinyin === '') {
+      alert('Please enter a word');
+      return;
+    }
+    await addDoc(collection(db, 'words'), {
+      english: english,
+      russian: russian,
+      chinese: chinese,
+      pinyin: pinyin,
+      completed: false,
+    });
+    setEnglish('');
+    setRussian('');
+    setChinese('');
+    setPinyin('');
+  };
+
+ 
+
+
+  return (
+    <div>
+      <form onSubmit={createWord} className={style.form}>
+        <input
+          value={english}
+          onChange={(e) => setEnglish(e.target.value)}
+          className={style.input}
+          type="text"
+          placeholder="English word"
+        />
+
+        <input
+          value={russian}
+          onChange={(e) => setRussian(e.target.value)}
+          className={style.input}
+          type="text"
+          placeholder="Russian word"
+        />
+
+        <input
+          value={chinese}
+          onChange={(e) => setChinese(e.target.value)}
+          className={style.input}
+          type="text"
+          placeholder="Chinese word"
+        />
+
+        <input
+          value={pinyin}
+          onChange={(e) => setPinyin(e.target.value)}
+          className={style.input}
+          type="text"
+          placeholder="Pinyin"
+        />
+
+        <button className={style.button}>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewWordForm;
