ee517f07ac2792e6f57ab0bc26523d0de82cec08
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-11-04 16:20:58 -0400

    october firebase update shaft1122

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,19 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDMEXxhW3Uhh3uHqLAhM4Pv9ytTefI9-V4",
-  authDomain: "online-words-dictionary.firebaseapp.com",
-  projectId: "online-words-dictionary",
-  storageBucket: "online-words-dictionary.appspot.com",
-  messagingSenderId: "523262205153",
-  appId: "1:523262205153:web:2469cc18b86fa1a97faba0"
+  apiKey: "AIzaSyDsDOPYKk3Gy8mZ04QO_DHKPk7y-wS9upE",
+  authDomain: "shaft1122-dictionary.firebaseapp.com",
+  projectId: "shaft1122-dictionary",
+  storageBucket: "shaft1122-dictionary.appspot.com",
+  messagingSenderId: "972298872861",
+  appId: "1:972298872861:web:5be6d357b960757bc23ef4"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
+
+
+
+
+
