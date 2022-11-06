7d59a4a5995f7e61ac44c55eee22c5d40e132d81
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-11-06 16:31:44 -0500

    basic copy functions

--- src/components/Words.jsx
+++ src/components/Words.jsx
@@ -28,26 +28,28 @@ const Word = ({ word, toggleComplete, deleteWord }) => {
             <tr className={style.tr}>
               <th className="w-2">
                 <input
-                className="h-4 w-3 rounded-xl"
+                className="h-5 w-5 rounded-xl"
                   onChange={() => toggleComplete(word)}
                   type="checkbox"
                   checked={word.completed ? "checked" : ""}
                 />
               </th>
+
               <th
-                onClick={() => toggleComplete(word)}
+                // onClick={() => toggleComplete(word)}
                 className={word.completed ? style.textComplete : style.textEn}
               >
                 {word.english}
               </th>
+              
               <th
-                onClick={() => toggleComplete(word)}
+                // onClick={() => toggleComplete(word)}
                 className={word.completed ? style.textComplete : style.textRu}
               >
                 {word.russian}
               </th>
               <th
-                onClick={() => toggleComplete(word)}
+                // onClick={() => toggleComplete(word)}
                 className={word.completed ? style.textComplete : style.textCn}
               >
                 {word.chinese}  {word.pinyin}
